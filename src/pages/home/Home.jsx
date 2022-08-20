b19e29df95a350a07d389c19e2afc4f5692f35e5
RogersBobMunyenga <b.munyenga@gmail.com>
2022-08-21 00:37:24 +0300

    featured component initial

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import "./home.scss"
 // import AcUnitIcon from '@mui/icons-material/AcUnit';
 import Navbar from "../../components/navbar/Navbar";
+import Featured from '../../components/featured/Featured';
 
 
 const Home = () => {
   return (
       <div className='home'>
-          <Navbar/>
+      <Navbar />
+      <Featured />
+      
           
     </div>
   )
