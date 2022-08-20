babdc100169958ea27dc89a6491204aa41ae694b
RogersBobMunyenga <b.munyenga@gmail.com>
2022-08-21 00:20:50 +0300

    navbar final

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,10 +2,16 @@ import SearchIcon from '@mui/icons-material/Search';
 import "./navbar.scss";
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import { useState } from 'react';
 
 const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll = null);
+  }
   return (
-      <div className="navbar">
+      <div className="navbar scrolled : navbar">
           <div className="container">
                <div className="left">
           <img
@@ -19,14 +25,20 @@ const Navbar = () => {
           <span>My List</span>
         </div>
         <div className="right">
-          <SearchIcon />
+          <SearchIcon  className='icon' />
           <span>KID</span>
-          <NotificationsIcon />
+          <NotificationsIcon className='icon' />
           <img
             src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
             alt=""
           />
-          <ArrowDropDownIcon/>
+          <div className="profile">
+            <ArrowDropDownIcon className='icon' />
+            <div className="options">
+              <span>Settings</span>
+              <span>Logout</span>
+            </div>
+          </div>
 
             </div>
           </div>
